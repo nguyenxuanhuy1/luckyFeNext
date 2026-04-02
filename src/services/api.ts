64b4aa1192efde73cc81b7e975695a8046364f9e
Huy <xuanhuybn2403@gmail.com>
2026-04-03 00:28:01 +0700

    sua domain

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://quaymayman.online/api",
   headers: {
     "Content-Type": "application/json",
   },
